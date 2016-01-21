26af2766b81408a7230a04e25fb810c2f1ced059
bgrove1974 <bgroveben@gmail.com>
2016-01-21 04:02:21 -0800

    Add a personalized welcome message

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 MDN Learning the Web tutorial (part of it at least).
 
-I am here:
+I just finished this page:
 
 https://developer.mozilla.org/en-US/Learn/Getting_started_with_the_web/JavaScript_basics
+
+and next comes website publishing options and the JavaScript guide.
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 var myImage = document.querySelector('img');
-
+// Switch between two images when the image is clicked
 myImage.onclick = function() {
   var mySource = myImage.getAttribute('src');
   if (mySource === 'images/firefox-icon.png') {
@@ -7,4 +7,24 @@ myImage.onclick = function() {
   } else {
     myImage.setAttribute('src','images/firefox-icon.png');
   }
+}
+// Get references to button in index.html and store them in variables
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+// Set a personalized greeting
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome to the Mozilla MDN Tutorial, ' + myName;
+}
+// Set up the app when it first loads to store the user's name (initialization code)
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome to the Mozilla MDN Tutorial, ' + storedName;
+}
+// Set event handler that allows user to set a new name
+myButton.onclick = function() {
+  setUserName();
 }
